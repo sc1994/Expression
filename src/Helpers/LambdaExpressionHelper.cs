7245e7c3ada0a5986f02efef8a354750d8b9d686
suncheng <sunchengsuncheng@gmail.com>
2017-12-19 14:39:21 +0800

    加入可能出现的嵌套表达式支持

---
+++ src/Helpers/LambdaExpressionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ExpressionHelper.Model;
+
+namespace ExpressionHelper.Helpers
+{
+    public class LambdaExpressionHelper : BaseExpressionHelper<LambdaExpression>
+    {
+        public override void AddWhere(LambdaExpression exp, ExpressionInfo arg)
+        {
+            HelperTool.AddWhere(exp.Body, arg);
+        }
+    }
+}
